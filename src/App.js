d758153756f001e0e99a04e2b866c728e0c316c0
luyifanwork <65984397+luyifanwork@users.noreply.github.com>
2020-06-20 21:58:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  state = {
+    loading: true,
+    info: [],
+  };
+
+  async componentDidMount(){
+    const url = "http://localhost:8000/";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({info: data.Artifacts, loading: false});
+    //console.log(data);
+  }
+
+  render(){
+    if (this.state.loading){
+      return <div>loading...</div>;
+    }
+    if (!this.state.info){
+      return <div>No such Artifacts</div>;
+    }
+    return (
+      <div> 
+        {this.state.info.map(Artifacts => (
+          <div key = {Artifacts.uid}>
+            <div>{Artifacts.title}</div>
+            <div>{Artifacts.details}</div>
+            <div>{Artifacts.uid}</div>
+            <div>{Artifacts.artifactType}</div>
+            <div>{Artifacts.slug}</div>
+          </div>
+        ))}
+      </div>
+      );
+  }
 }
-
-export default App;
